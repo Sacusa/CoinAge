753616ff3e7697006a15ba7ac8aa411a0926dfe7
Sudhanshu Gupta <Sacusa@users.noreply.github.com>
2018-02-18 13:53:33 +0530

    Update Stock.java

--- src/Stock.java
+++ src/Stock.java
@@ -1,70 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.coinage;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-
 /**
- * The information about an individual stock at a particular time instance is stored 
- * in this class.
- * 
- * @author tanmaypatil
- */
-public class Stock 
-{
-    /** symbol contains the stock symbol. For example, Microsoft symbol will be "MSFT". */
-    private final String symbol;
-    
-    /** dateTime represents the date and time the object contains information for. */
-    private final String dateTime;
-    
-    /**
-     * values contains the detailed information about the stock like openValue, 
-     * highValue, lowValue, closeValue and volume, each of which can be referred to using the
-     * string "open", "high", "low", "close" and "volume" respectively.
-     */
-    private final Map<String, Double> values;
-    
-    /**
-     * @param symbol
-     * @param dateTime
-     * @param values
-     */
-    public Stock(String symbol, String dateTime, Map<String, Double> values) {
-        this.symbol = symbol;
-        this.dateTime = dateTime;
-        this.values = new HashMap<> (values);
-    }
-    
-    /**
-     * Returns a HashMap which contains a detailed information about the stock.
-     * @return values
-     */
-    public Map<String, Double> getValues ()
-    {
-        return new HashMap<> (values);
-    }
+* The information about an individual stock at a particular time instance is stored in this class.
+* Objects of this class are immutable. Consequently, only getter methods are present.
+* 
+* @author tanmaypatil
+*/
+public class Stock  {
+  /** symbol contains the stock symbol. For example, Microsoft symbol will be "MSFT". */
+  private final String symbol;
+  
+  /** dateTime represents the date and time the object contains information for. */
+  private final String dateTime;
+  
+  /** values contains the detailed information about the stock. */
+  private final Map<String, Double> values;
+  
+  /**
+   * Construct a Stock object.
+   * 
+   * @param symbol   The symbol the object corresponds to.
+   * @param dateTime The date and time the object represents.
+   * @param values   The stock values the object needs to contain. The information includes
+   *                 openValue, highValue, lowValue, closeValue and volume, each of which should
+   *                 be accessible using the strings "open", "high", "low", "close" and "volume"
+   *                 respectively.
+   */
+  public Stock(String symbol, String dateTime, Map<String, Double> values) {
+    this.symbol = symbol;
+    this.dateTime = dateTime;
+    this.values = new HashMap<>();
 
-    /**
-     * Returns a String which contains the stock symbol
-     * @return symbol 
-     */
-    public String getSymbol() 
-    {
-        return symbol;
-    }
-    /**
-     * Returns a String which contains the date and time of the stock
-     * @return dateTime 
-     */
-    public String getDateTime() 
-    {
-        return dateTime;
-    }
+    // load stock value from 'values' if it contains the key. Else, set that value to 0.
+    List<String> keys = Arrays.asList("open", "high", "low", "close", "volume");
     
+    for (String key : keys) {
+      if (values.containsKey(key)) {
+        this.values.put(key, values.get(key));
+      } else {
+        this.values.put(key, 0d);
+      }
+    }
+  }
+  
+  /**
+   * Returns a HashMap which contains a detailed information about the stock, like openValue,
+   * highValue, lowValue, closeValue and volume, each of which can be referred to using the string
+   * "open", "high", "low", "close" and "volume" respectively.
+   * 
+   * @return values
+   */
+  public Map<String, Double> getValues() {
+    return new HashMap<>(values);
+  }
+
+  /**
+   * Returns a String which contains the stock symbol.
+   * 
+   * @return symbol 
+   */
+  public String getSymbol() {
+    return symbol;
+  }
+
+  /**
+   * Returns a String which contains the date and time of the stock.
+   * 
+   * @return dateTime 
+   */
+  public String getDateTime() {
+    return dateTime;
+  }
+  
 }
